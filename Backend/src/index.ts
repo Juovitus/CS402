87c89adacbb387339ffb2aebadc24178b17aeafe
Evan-Peterson <4epeterson99@gmail.com>
2022-02-09 11:07:23 -0600

    changed to typescript, added typescript dependencies, added dist folder to gitingore

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,11 +1,12 @@
-const express = require("express");
+import express from "express";
 const app = express();
 const port = 8080;
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello TypeScript World!");
 });
 
 app.listen(port, () => {
+    // tslint:disable-next-line:no-console
     console.log(`server started at http://localhost:${ port }`);
 });
