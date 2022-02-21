a14249a97459496ece3aea0ae50c2d4d4e5995f7
Aarodynamics3 <hanrahanaaron@gmail.com>
2022-02-20 22:29:37 -0600

    Created components folder and started work with navbar.

---
+++ frontend/src/components/navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../../App.css'
+
+class Navbar extends React.Component {
+    render(): React.ReactNode {
+        return (
+            <div className="nav">
+                Navbar
+            </div>
+        );
+    }
+}
+
+export default Navbar;
