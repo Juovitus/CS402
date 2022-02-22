9b757288edd56e451fc6b52e50e61d18e5f2b42f
Evan-Peterson <4epeterson99@gmail.com>
2022-02-22 15:30:31 -0600

    fixed auth url to have correct redirect uri

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -17,7 +17,7 @@ routes.register( app );
 
 app.get("/", (req, res) => {
     // res.send("Hello TypeScript World!");
-    res.redirect("https://git.las.iastate.edu/oauth/authorize?client_id=fc94a7e013a5f5ae499b5fb8cebe8cf1480d21845933231572c6ed4f689167fc&redirect_uri=http://localhost:8080/home&response_type=code&state=FT52maX47nRH5ubBpraPU30CrgnU2ZX0&scope=read_user+api+read_api+read_repository&code_challenge=M5oPBP6RmJPh5QsGtx6ptVH7S2yjqt8sum96jBCyhZg&code_challenge_method=plain");
+    res.redirect("https://git.las.iastate.edu/oauth/authorize?client_id=fc94a7e013a5f5ae499b5fb8cebe8cf1480d21845933231572c6ed4f689167fc&redirect_uri=http://coms-402-sd-22.class.las.iastate.edu:8080/home/&response_type=code&state=FT52maX47nRH5ubBpraPU30CrgnU2ZX0&scope=read_user+api+read_api+read_repository&code_challenge=M5oPBP6RmJPh5QsGtx6ptVH7S2yjqt8sum96jBCyhZg&code_challenge_method=plain");
     // res.send(req.params);
 });
 
@@ -31,7 +31,7 @@ app.get("/home", (req, res) => {
     const clientid = "fc94a7e013a5f5ae499b5fb8cebe8cf1480d21845933231572c6ed4f689167fc";
     const clientsecret = "15eb944a1a96401bb4add5fc16d5a3d7ae4026074fbad479926a4e2b3d601aa5";
     const granttype = "authorization_code";
-    const redirecturi = "http://coms-402-sd-22.class.las.iastate.edu:8080/home";
+    const redirecturi = "http://coms-402-sd-22.class.las.iastate.edu:8080/home/";
     const codeverifier = "M5oPBP6RmJPh5QsGtx6ptVH7S2yjqt8sum96jBCyhZg";
 
     const authURL = "https://git.las.iastate.edu/oauth/token?client_id=fc94a7e013a5f5ae499b5fb8cebe8cf1480d21845933231572c6ed4f689167fc&client_secret=15eb944a1a96401bb4add5fc16d5a3d7ae4026074fbad479926a4e2b3d601aa5&code=" + code + "&grant_type=authorization_code&redirect_uri=http://coms-402-sd-22.class.las.iastate.edu:8080/home/&code_verifier=M5oPBP6RmJPh5QsGtx6ptVH7S2yjqt8sum96jBCyhZg"
