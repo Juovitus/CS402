63b34064d804040bc9f3024fb6b4fee578e0a116
Evan-Peterson <4epeterson99@gmail.com>
2022-02-24 20:12:47 -0600

    added user get and project get

--- Backend/src/routes/api.ts
+++ Backend/src/routes/api.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import axios from "axios";
 
 
 export const register = ( app: express.Application ) => {
@@ -6,4 +7,30 @@ export const register = ( app: express.Application ) => {
     app.get("/user", (req, res) => {
         res.send("response");
     });
+
+    // Returns all projects from a user
+    app.get("/projects/:userID", (req, res) => {
+
+        // query the database to get the user access token
+        const token = 0;   // TEMP VALUE
+
+        const config = {
+            headers: {Authorization: `Bearer ${token}`}
+        };
+
+        const query = 'https://git.las.iastate.edu/api/v4/projects?owned=true&simple=true';
+
+        axios.get(query, config).then( (val: any) => {
+            res.send(val.data);
+        });
+    });
+
+    // Gets latest commit from a project
+    // userID: id of the user with access to the project
+    // projectID: id of the project
+    app.get("/projects/commit/:userID/:projectID", (req, res) => {
+
+    });
+
+    // app.post('/user/')
 };
