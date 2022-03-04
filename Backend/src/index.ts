6f73cb07cb061d6b452a06df950532b6c1395d34
Evan-Peterson <4epeterson99@gmail.com>
2022-03-04 15:58:18 -0600

    added access control

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -82,6 +82,25 @@ app.get("/home", (req, res) => {
         console.log("test received");
     });
 
+app.use( (req, res, next) => {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', "true");
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.listen(port, () => {
     // tslint:disable-next-line:no-console
     console.log(`server started at ${port}`);
