2cd51ade7203464e38171ecb5b04c953b6081530
Aarodynamics3 <hanrahanaaron@gmail.com>
2022-03-05 19:56:57 -0600

    Removed unnecessary component in navbar

--- frontend/src/components/navbar/NavItem.tsx
+++ frontend/src/components/navbar/NavItem.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Icon from '@mdi/react'
-import { NavItemText } from "./NavItemText";
+import Icon from '@mdi/react';
+import styled from 'styled-components';
 
 interface Props {
     id: string
@@ -18,7 +18,14 @@ export const NavItem: React.FC<Props> = ({text, icon, id, mount}) => {
                 size='36px'
                 className="nav-item-icon"
                 />
-            {mount ? <NavItemText text={text} /> : null}
+            {mount ? <NavItemText>{text}</NavItemText> : null}
         </div>
     );
 };
+
+const NavItemText = styled.span`
+    font-size: 28px;
+    font-weight: 600;
+    margin-right: 50px;
+    margin-left: 20px;
+`;
