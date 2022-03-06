da6a8c3aa1ed7a7e37a7a5eee71d8891cb6d7ecb
Aarodynamics3 <hanrahanaaron@gmail.com>
2022-03-05 19:48:06 -0600

    Added styled-components and converted the ProjectPreview component to it.

--- frontend/src/components/projectpreview/ProjectPreview.tsx
+++ frontend/src/components/projectpreview/ProjectPreview.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Top } from "./Top"
-import { Bottom } from "./Bottom"
+import { Top } from "./Top";
+import { Bottom } from "./Bottom";
+import styled from 'styled-components';
 
 interface Props {
     key: any
@@ -9,13 +10,25 @@ interface Props {
 
 export const ProjectPreview: React.FC<Props> = () => {
     const pName = 'MedHub'
+    const dDate = 4
 
     return (
-        <div className='ProjectPreview'>
+        <Container>
             <Top
                 projectName={pName}
+                demoDate={dDate}
             />
             <Bottom/>
-        </div>
+        </Container>
     );
 }
+
+const Container = styled.div`
+    width: 300px;
+    height: 200px;
+    overflow: hidden;
+    display: grid;
+    grid-template-rows: min-content 1fr;
+    color: rgb(44, 44, 44);
+    font-size: 14px;
+`;
--- frontend/src/components/projectpreview/Top.tsx
+++ frontend/src/components/projectpreview/Top.tsx
@@ -1,37 +1,52 @@
 import React from "react";
+import styled from 'styled-components';
 
 interface Props {
     projectName: string
+    demoDate: Number
 }
 
-export const Top: React.FC<Props> = ({projectName}) => {
+export const Top: React.FC<Props> = ({projectName, demoDate}) => {
     return (
-        <div className="Top">
-            {ProjectName(projectName)}
+        <Container>
+            <ProjectName>{projectName}</ProjectName>
             {TAList}
-            {BottomRow(1)}
-        </div>
+            <BottomRow>
+                {StudentList}
+                <DemoDate>Demo - {demoDate} days</DemoDate>
+            </BottomRow>
+        </Container>
     );
 }
 
-/* 
-Not sure if this will need to be a FC in the future,
-leaving as normal fn for now.
-*/
-const ProjectName = (name:string) => {
-    return (
-        <div id="project-name">{name}</div>
-    );
-}
+const Container = styled.div`
+    border-radius: 10px 10px 0 0;
+    box-sizing: border-box;
+    background-color: #F6F6F6;
+    /* top right bottom left */
+    padding: 10px 7px 10px 7px;
+    box-shadow: rgba(0, 0, 0, 0.26) 0px 1px 4px;
+    z-index: 1;
+`;
 
-const BottomRow = (days:Number) => {
-    return (
-        /* bottom-row is flex container */
-        <div className="bottom-row">
-            <div id="student-list">Students</div>
-            <div id="demo-date">Demo - {days} days</div>
-        </div>
-    );
-}
+const ProjectName = styled.div`
+    font-weight: 500;
+    font-size: 18px;
+`;
 
+const BottomRow = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin: 4px 0;
+`;
+
+const DemoDate = styled.div`
+    border-radius: 10px;
+    padding: 1px 8px;
+    background-color: #ef946c;
+    box-shadow: rgba(0, 0, 0, 0.26) 0px 1px 4px;
+`;
+
+//TODO make student list and ta list their own fn component
 const TAList = <div id="ta-name">TA</div>
+const StudentList = <div id="student-list">Students</div>
